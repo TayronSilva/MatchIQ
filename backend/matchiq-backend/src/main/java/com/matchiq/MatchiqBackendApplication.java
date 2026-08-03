6d29c255423e60d853fa3ce2669660342f9a2da3
TayronSilva <ttayronne.silva@gmail.com>
2026-08-03 19:49:11 -0300

    chore(backend): initialize Spring Boot project

---
+++ backend/matchiq-backend/src/main/java/com/matchiq/MatchiqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.matchiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchiqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchiqBackendApplication.class, args);
+	}
+
+}
---
+++ backend/matchiq-backend/src/test/java/com/matchiq/MatchiqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchiqBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
