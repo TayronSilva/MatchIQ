90f52419882b758c3e4927c4410a7a8e19e2d089
TayronSilva <ttayronne.silva@gmail.com>
2026-08-04 18:28:43 -0300

    feat(user): create UserController

---
+++ backend/matchiq-backend/src/main/java/com/matchiq/user/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.matchiq.user.controller;
+
+import com.matchiq.user.dto.CreateUserRequest;
+import com.matchiq.user.dto.UserResponse;
+import com.matchiq.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse create(@Valid @RequestBody CreateUserRequest request) {
+        return userService.create(request);
+    }
+}
