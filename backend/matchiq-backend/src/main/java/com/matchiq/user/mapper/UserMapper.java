2f31b6c82bb1bf59da6182cd47a32658759dd6fa
TayronSilva <ttayronne.silva@gmail.com>
2026-08-04 12:17:49 -0300

    feat(user): implement UserMapper

---
+++ backend/matchiq-backend/src/main/java/com/matchiq/user/mapper/UserMapper.java
@@ -0,0 +1,44 @@
+package com.matchiq.user.mapper;
+
+import com.matchiq.user.domain.User;
+import com.matchiq.user.dto.CreateUserRequest;
+import com.matchiq.user.dto.UserResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public User toEntity(CreateUserRequest request) {
+        if(request == null) {
+            return null;
+        }
+
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setLanguage(request.getLanguage());
+        user.setTheme(request.getTheme());
+        user.setNotificationEnabled(request.isNotificationEnabled());
+
+        return user;
+    }
+
+    public UserResponse toResponse(User user){
+        if(user == null) {
+            return null;
+        }
+
+        UserResponse response = new UserResponse();
+        response.setId(user.getId());
+        response.setName(user.getName());
+        response.setEmail(user.getEmail());
+        response.setLanguage(user.getLanguage());
+        response.setTheme(user.getTheme());
+        response.setNotificationEnabled(user.isNotificationEnabled());
+        response.setCreatedAt(user.getCreatedAt());
+        response.setUpdatedAt(user.getUpdatedAt());
+
+        return response;
+    }
+
+}
